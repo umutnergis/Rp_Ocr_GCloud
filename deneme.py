56180340974af3c202b78b8344c0a0e4de020ecc
umut nergis <iumut.nergis@std.hku.edu.tr>
2023-08-22 17:35:17 +0300

    ocr.py updateted

--- deneme.py
+++
@@ -1,7 +0,0 @@
-import os
-from google.cloud import vision
-from google.cloud.vision_v1 import types
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'smiling-audio-396618-2b29f033f036.json'
-
-client = vision.ImageAnnotatorClient()
